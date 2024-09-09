f4dd4bbbedb6a6dcee4c57b7b7edd755593cc72c
ankteaaa <90596636+ankteaaa@users.noreply.github.com>
2024-09-09 07:32:41 +0200

    Sammenligning af to tal

---
+++ Sammenligning af to tal/script.js
@@ -0,0 +1,10 @@
+let tal1 = 1;
+let tal2 = 2;
+
+if (tal1 < tal2){
+    alert("tal1 er mindre end tal2");
+}else if (tal1 == tal2){
+    alert("tal1 lig med tal2");
+}else{
+    alert("tal1 er større end tal2");
+}
